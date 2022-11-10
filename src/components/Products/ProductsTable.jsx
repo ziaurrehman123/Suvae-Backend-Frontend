775878da39cba15cd137d46f4f1332f237e664aa
ziaurrehman123 <ziaurrehman1122111@gmail.com>
2022-11-11 01:01:47 +0500

    product table added

--- src/components/Products/ProductsTable.jsx
+++ src/components/Products/ProductsTable.jsx
@@ -1,16 +1,37 @@
 import React from "react";
-import { Typography, Box, Grid, Button } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 import Tooltip from "@mui/material/Tooltip";
 import productImage from "../../assets/images/productImage.png";
 import TablePagination from "@mui/material/TablePagination";
-import Modal from "@mui/material/Modal";
-import CloseIcon from "@mui/icons-material/Close";
-import Divider from "@mui/material/Divider";
 import SearchSection from "../Home/SearchSection";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 import { styled } from "@mui/material/styles";
+import {
+  Divider,
+  Box,
+  Button,
+  Grid,
+  Link,
+  Typography,
+  TextField,
+  Tabs,
+  TableHead,
+  Tab,
+  Modal,
+  CircularProgress,
+  TableBody,
+  Table,
+  TableCell,
+  TableContainer,
+  InputLabel,
+  FormControl,
+  MenuItem,
+  Select,
+  TableRow,
+  Paper,
+  Fade,
+} from "@mui/material";
 
 const IOSSwitch = styled((props) => (
   <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
@@ -110,121 +131,123 @@ export default function ProductsTable() {
         </Typography>
         <SearchSection />
       </Box>
-      <Box
-        sx={{
-          backgroundColor: "white",
-          paddingTop: "20px",
-          borderRadius: "20px",
-          paddingBottom: "20px",
-        }}
-      >
-       
-        <Box sx={{ width: "95%", margin: "auto", paddingTop: "20px" }}>
-          <Box
-            sx={{
-              backgroundColor: "#F6F7F8",
-              paddingTop: "20px",
-              paddingBottom: "20px",
-              borderRadius: "3px",
-            }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                <Typography
-                  sx={{
-                    fontWeight: "400",
-                    fontSize: "16px",
-                    textAlign: "center",
-                    color: "#8B8FA3",
-                  }}
-                >
-                  Product
-                </Typography>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                <Box sx={{ display: "flex", width: {xs:"53%", sm:"53%", md:"50%", lg:"50%", xl:"50%"}, margin: "auto" }}>
+   
+      <Box>
+        <TableContainer
+          component={Paper}
+          sx={{
+            padding: {
+              xs: "10px",
+              sm: "10px",
+              md: "40px",
+              lg: "40px",
+              xl: "40px",
+            },
+            marginTop:"40px",
+            width:"95%",
+            margin:"auto",
+            borderRadius:"20px",
+          }}
+        >
+         
+          <Table aria-label="simple table" sx={{ overflowX: "hidden" }}>
+            <TableHead sx={{ backgroundColor: "#F6F7F8" }}>
+              <TableRow>
+                <TableCell sx={{ fontSize: "16px", color: "#8B8FA3" }}>
                   <Typography
                     sx={{
                       fontWeight: "400",
                       fontSize: "16px",
-
+                      textAlign: "center",
                       color: "#8B8FA3",
                     }}
                   >
-                    Form Submissions
+                    Product
                   </Typography>
-                  <Box>
-                    <Tooltip
-                      title="Enable or Disable submissions for specific products"
-                      placement="top-start"
-                    >
-                      <InfoIcon
-                        sx={{
-                          color: "#7335FD",
-                          marginLeft: "10px",
-                          marginTop: "0px",
-                        }}
-                      />
-                    </Tooltip>
-                  </Box>
-                </Box>
-              </Grid>
+                </TableCell>
+                <TableCell sx={{ fontSize: "16px", color: "#8B8FA3" }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                     width:"50%", margin:"auto",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontWeight: "400",
+                        fontSize: "16px",
 
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                <Box sx={{ display: "flex", width: {xs:"53%", sm:"53%", md:"50%", lg:"50%", xl:"50%"}, margin: "auto" }}>
+                        color: "#8B8FA3",
+                      }}
+                    >
+                      Form Submissions
+                    </Typography>
+                    <Box>
+                      <Tooltip
+                        title="Enable or Disable submissions for specific products"
+                        placement="top-start"
+                      >
+                        <InfoIcon
+                          sx={{
+                            color: "#7335FD",
+                            marginLeft: "10px",
+                            marginTop: "0px",
+                          }}
+                        />
+                      </Tooltip>
+                    </Box>
+                  </Box>
+                </TableCell>
+                <TableCell sx={{ fontSize: "16px", color: "#8B8FA3" }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      width:"50%", margin:"auto",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontWeight: "400",
+                        fontSize: "16px",
+                        marginLeft: "20px",
+                        color: "#8B8FA3",
+                      }}
+                    >
+                      Form Incentive
+                    </Typography>
+                    <Box>
+                      <Tooltip
+                        title="Incentives are sent to your customer after completion of our form."
+                        placement="top-start"
+                      >
+                        <InfoIcon
+                          sx={{
+                            color: "#7335FD",
+                            marginLeft: "10px",
+                            marginTop: "0px",
+                          }}
+                        />
+                      </Tooltip>
+                    </Box>
+                  </Box>
+                </TableCell>
+                <TableCell sx={{ fontSize: "16px", color: "#8B8FA3" }}>
                   <Typography
                     sx={{
                       fontWeight: "400",
                       fontSize: "16px",
-                      marginLeft: "20px",
+                      textAlign: "center",
                       color: "#8B8FA3",
                     }}
                   >
-                   Form Incentive
+                    Reviews Collected
                   </Typography>
-                  <Box>
-                    <Tooltip
-                      title="Incentives are sent to your customer after completion of our form."
-                      placement="top-start"
-                    >
-                      <InfoIcon
-                        sx={{
-                          color: "#7335FD",
-                          marginLeft: "10px",
-                          marginTop: "0px",
-                        }}
-                      />
-                    </Tooltip>
-                  </Box>
-                </Box>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                <Typography
-                  sx={{
-                    fontWeight: "400",
-                    fontSize: "16px",
-                    textAlign: "center",
-                    color: "#8B8FA3",
-                  }}
-                >
-                  Reviews Collected
-                </Typography>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Box sx={{ width: "95%", margin: "auto", paddingTop: "20px" }}>
-          <Box
-            sx={{
-              backgroundColor: "",
-              paddingTop: "20px",
-              paddingBottom: "20px",
-              borderRadius: "3px",
-            }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                <Box sx={{ display: "flex", width: "66%", margin: "auto" }}>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>     <Box sx={{ display: "flex", width:"60%", margin:"auto"  }}>
                   <Box
                     component="img"
                     sx={{
@@ -237,20 +260,16 @@ export default function ProductsTable() {
                   />
                   <Box>
                     <Typography>
-                      Moon Lamp RGB Glow Remote Controlled...
+                      Moon Lamp RGB 
                     </Typography>
                   </Box>
-                </Box>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                <Box sx={{ textAlign: "center" }}>
+                </Box></TableCell>
+                <TableCell> <Box sx={{ textAlign: "center" }}>
                   <FormControlLabel
                     control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
                   />
-                </Box>
-              </Grid>
-
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
+                </Box></TableCell>
+                <TableCell>
                 <Box sx={{ textAlign: "center" }}>
                   <Button
                     variant="contained"
@@ -267,37 +286,16 @@ export default function ProductsTable() {
                     View
                   </Button>
                 </Box>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
+                </TableCell>
+                <TableCell>
                 <Box sx={{ textAlign: "center" }}>
                   <Typography>0</Typography>
                 </Box>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-
-        <Box
-          sx={{
-            width: "95%",
-            height: "1px",
-            backgroundColor: "#DDE1E9",
-            marginTop: "15px",
-            margin: "auto",
-          }}
-        ></Box>
-         <Box sx={{ width: "95%", margin: "auto", paddingTop: "20px" }}>
-          <Box
-            sx={{
-              backgroundColor: "",
-              paddingTop: "20px",
-              paddingBottom: "20px",
-              borderRadius: "3px",
-            }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                <Box sx={{ display: "flex", width: "66%", margin: "auto" }}>
+                </TableCell>
+              </TableRow>
+        
+              <TableRow>
+                <TableCell>     <Box sx={{ display: "flex", width:"60%", margin:"auto"  }}>
                   <Box
                     component="img"
                     sx={{
@@ -310,34 +308,23 @@ export default function ProductsTable() {
                   />
                   <Box>
                     <Typography>
-                      Moon Lamp RGB Glow Remote Controlled...
+                      Moon Lamp RGB 
                     </Typography>
                   </Box>
-                </Box>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
-                <Box sx={{ textAlign: "center" }}>
-                <Tooltip
-                      title="You must setup a Form Incentive
-                      before enabling this product.
-                      "
-                      placement="top-start"
-                    >
+                </Box></TableCell>
+                <TableCell> <Box sx={{ textAlign: "center" }}>
                   <FormControlLabel
-                    control={<IOSSwitch sx={{ m: 1 }}  />}
+                    control={<IOSSwitch sx={{ m: 1 }} />}
                   />
-                  </Tooltip>
-                </Box>
-              </Grid>
-
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
+                </Box></TableCell>
+                <TableCell>
                 <Box sx={{ textAlign: "center" }}>
                   <Button
                     variant="outlined"
                     sx={{
                       color: "#8B8FA3",
-                      border:"1px solid #8B8FA3",
-                    
+                      border: "1px solid #8B8FA3",
+
                       borderRadius: "2px",
                       marginTop: "10px",
                       textTransform: "capitalize",
@@ -348,37 +335,17 @@ export default function ProductsTable() {
                     Setup
                   </Button>
                 </Box>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6} lg={3} xl={3}>
+                </TableCell>
+                <TableCell>
                 <Box sx={{ textAlign: "center" }}>
                   <Typography>0</Typography>
                 </Box>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-
-        <Box
-          sx={{
-            width: "95%",
-            height: "1px",
-            backgroundColor: "#DDE1E9",
-            marginTop: "15px",
-            margin: "auto",
-          }}
-        ></Box>
-
-        <Box
-          sx={{
-            width: "95%",
-            height: "1px",
-            backgroundColor: "#DDE1E9",
-            marginTop: "15px",
-            margin: "auto",
-          }}
-        ></Box>
-
-        <Box sx={{ width: "97%", margin: "auto", paddingTop: "20px" }}>
+                </TableCell>
+              </TableRow>
+        
+            </TableBody>
+          </Table>
+          <Box sx={{ width: "97%", margin: "auto", paddingTop: "20px" }}>
           <TablePagination
             component="div"
             count={100}
@@ -388,6 +355,8 @@ export default function ProductsTable() {
             onRowsPerPageChange={handleChangeRowsPerPage}
           />
         </Box>
+        </TableContainer>
+        
       </Box>
     </>
   );
